9406d2f786f834e319b4d283227334cd4c847d1f
Travis Westcott <westcott2436@msn.com>
2024-03-15 12:30:13 -0500

    Starting fresh with a new database, entities, context.

--- Fintasticfish.Data/Entities/FishesFood.cs
+++ Fintasticfish.Data/Entities/FishesFood.cs
@@ -5,11 +5,13 @@ using System.Collections.Generic;
 
 namespace FintasticFish.Data.Entities;
 
-public partial class WaterType
+public partial class FishesFood
 {
-    public int Id { get; set; }
+    public int FishId { get; set; }
 
-    public string Name { get; set; }
+    public int FoodId { get; set; }
 
-    public virtual ICollection<Fish> Fish { get; set; } = new List<Fish>();
+    public virtual Fish Fish { get; set; }
+
+    public virtual Food Food { get; set; }
 }
--- Fintasticfish.Data/Entities/OrderStatus.cs
+++ Fintasticfish.Data/Entities/OrderStatus.cs
@@ -5,11 +5,11 @@ using System.Collections.Generic;
 
 namespace FintasticFish.Data.Entities;
 
-public partial class WaterType
+public partial class OrderStatus
 {
     public int Id { get; set; }
 
     public string Name { get; set; }
 
-    public virtual ICollection<Fish> Fish { get; set; } = new List<Fish>();
+    public virtual ICollection<Order> Orders { get; set; } = new List<Order>();
 }
