a5928eacfc582d1bcc30794aed6a476e474385bd
Travis Westcott <westcott2436@msn.com>
2024-03-18 13:03:21 -0500

    Added the supplier controller and ensured inforation is human readable.

---
+++ FintasticFish.Web/Controllers/SuppliersController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FintasticFish.Data;
+using FintasticFish.Data.Entities;
+
+namespace FintasticFish.Web.Controllers
+{
+    public class SuppliersController : Controller
+    {
+        private readonly FintasticFishContext _context;
+
+        public SuppliersController(FintasticFishContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Suppliers
+        public async Task<IActionResult> Index()
+        {
+            var fintasticFishContext = _context.Suppliers.Include(s => s.Country).Include(s => s.SupplierType);
+            return View(await fintasticFishContext.ToListAsync());
+        }
+
+        // GET: Suppliers/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var supplier = await _context.Suppliers
+                .Include(s => s.Country)
+                .Include(s => s.SupplierType)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (supplier == null)
+            {
+                return NotFound();
+            }
+
+            return View(supplier);
+        }
+
+        // GET: Suppliers/Create
+        public IActionResult Create()
+        {
+            ViewData["CountryId"] = new SelectList(_context.Countries, "Id", "Name");
+            ViewData["SupplierTypeId"] = new SelectList(_context.SupplierTypes, "Id", "Name");
+            return View();
+        }
+
+        // POST: Suppliers/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,ContactName,Phone,Email,Website,SupplierTypeId,Notes,CountryId")] Supplier supplier)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(supplier);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CountryId"] = new SelectList(_context.Countries, "Id", "Name", supplier.CountryId);
+            ViewData["SupplierTypeId"] = new SelectList(_context.SupplierTypes, "Id", "Name", supplier.SupplierTypeId);
+            return View(supplier);
+        }
+
+        // GET: Suppliers/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var supplier = await _context.Suppliers.FindAsync(id);
+            if (supplier == null)
+            {
+                return NotFound();
+            }
+            ViewData["CountryId"] = new SelectList(_context.Countries, "Id", "Name", supplier.CountryId);
+            ViewData["SupplierTypeId"] = new SelectList(_context.SupplierTypes, "Id", "Name", supplier.SupplierTypeId);
+            return View(supplier);
+        }
+
+        // POST: Suppliers/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,ContactName,Phone,Email,Website,SupplierTypeId,Notes,CountryId")] Supplier supplier)
+        {
+            if (id != supplier.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(supplier);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SupplierExists(supplier.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CountryId"] = new SelectList(_context.Countries, "Id", "Name", supplier.CountryId);
+            ViewData["SupplierTypeId"] = new SelectList(_context.SupplierTypes, "Id", "Name", supplier.SupplierTypeId);
+            return View(supplier);
+        }
+
+        // GET: Suppliers/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var supplier = await _context.Suppliers
+                .Include(s => s.Country)
+                .Include(s => s.SupplierType)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (supplier == null)
+            {
+                return NotFound();
+            }
+
+            return View(supplier);
+        }
+
+        // POST: Suppliers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var supplier = await _context.Suppliers.FindAsync(id);
+            if (supplier != null)
+            {
+                _context.Suppliers.Remove(supplier);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SupplierExists(int id)
+        {
+            return _context.Suppliers.Any(e => e.Id == id);
+        }
+    }
+}
