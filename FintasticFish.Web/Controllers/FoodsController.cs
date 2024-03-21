b83dbe2ef12b61341bfda3bb073aa37e432305e5
Travis Westcott <westcott2436@msn.com>
2024-03-21 12:03:14 -0500

    Added foods controller, changed order of fields for readability.

---
+++ FintasticFish.Web/Controllers/FoodsController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FintasticFish.Data.Entities;
+
+namespace FintasticFish.Web.Controllers
+{
+    public class FoodsController : Controller
+    {
+        private readonly FintasticFishContext _context;
+
+        public FoodsController(FintasticFishContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Foods
+        public async Task<IActionResult> Index()
+        {
+            var fintasticFishContext = _context.Foods.Include(f => f.FoodType).Include(f => f.Mearsurement);
+            return View(await fintasticFishContext.ToListAsync());
+        }
+
+        // GET: Foods/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var food = await _context.Foods
+                .Include(f => f.FoodType)
+                .Include(f => f.Mearsurement)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (food == null)
+            {
+                return NotFound();
+            }
+
+            return View(food);
+        }
+
+        // GET: Foods/Create
+        public IActionResult Create()
+        {
+            ViewData["FoodTypeId"] = new SelectList(_context.FoodTypes, "Id", "Name");
+            ViewData["MearsurementId"] = new SelectList(_context.Measurements, "Id", "Name");
+            return View();
+        }
+
+        // POST: Foods/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Price,SalePrice,Size,Taxable,Stock,SaleStartDate,SaleEndDate,FoodTypeId,MearsurementId")] Food food)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(food);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["FoodTypeId"] = new SelectList(_context.FoodTypes, "Id", "Name", food.FoodTypeId);
+            ViewData["MearsurementId"] = new SelectList(_context.Measurements, "Id", "Name", food.MearsurementId);
+            return View(food);
+        }
+
+        // GET: Foods/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var food = await _context.Foods.FindAsync(id);
+            if (food == null)
+            {
+                return NotFound();
+            }
+            ViewData["FoodTypeId"] = new SelectList(_context.FoodTypes, "Id", "Name", food.FoodTypeId);
+            ViewData["MearsurementId"] = new SelectList(_context.Measurements, "Id", "Name", food.MearsurementId);
+            return View(food);
+        }
+
+        // POST: Foods/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Price,SalePrice,Size,Taxable,Stock,SaleStartDate,SaleEndDate,FoodTypeId,MearsurementId")] Food food)
+        {
+            if (id != food.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(food);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!FoodExists(food.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["FoodTypeId"] = new SelectList(_context.FoodTypes, "Id", "Name", food.FoodTypeId);
+            ViewData["MearsurementId"] = new SelectList(_context.Measurements, "Id", "Name", food.MearsurementId);
+            return View(food);
+        }
+
+        // GET: Foods/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var food = await _context.Foods
+                .Include(f => f.FoodType)
+                .Include(f => f.Mearsurement)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (food == null)
+            {
+                return NotFound();
+            }
+
+            return View(food);
+        }
+
+        // POST: Foods/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var food = await _context.Foods.FindAsync(id);
+            if (food != null)
+            {
+                _context.Foods.Remove(food);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool FoodExists(int id)
+        {
+            return _context.Foods.Any(e => e.Id == id);
+        }
+    }
+}
